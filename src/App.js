3d0e935547c80f585e83e5209124f0e68a094bc8
Akshar Mistry <mistry.aks@gmail.com>
2020-06-08 18:25:47 +0530

    Refactor folder structure

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Oh Yeah!!! Yay!!!
+          Learn React - Oh Yeah!!! Yay
         </a>
       </header>
     </div>
